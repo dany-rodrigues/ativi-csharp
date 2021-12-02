fe8e5ee1c36354859d6c5bbc0e28c44f789607f8
Perkles <danyellealvesoficial@gmail.com>
2021-12-02 19:59:34 -0300

    Primeiras atividades C-sharp curso Recode Pro

---
+++ exercício1/Program.cs
@@ -0,0 +1,29 @@
+﻿/*
+1-Crie uma aplicação console contemplando três variáveis inteiras, com nomes livres.
+
+2-Atribua valores iniciais às três.
+
+3-Imprima o resultado de cada uma delas em linhas distintas.
+
+
+4-Imprima o resultado utilizando operadores de comparação, a sua escolha, entre as duas primeiras variáveis.
+
+5-Imprima o resultado utilizando operadores de igualdade entre as duas últimas variáveis.
+
+6-Utilize operadores aritméticos distintos com a primeira e a última variáveis e em seguida imprima o novo 
+resultado das duas em linhas distintas.
+*/
+
+int a = 17;
+int b = 25;
+int c = 32;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+
+Console.WriteLine(a < b);
+Console.WriteLine(b == c);
+
+Console.WriteLine(++a);
+Console.WriteLine(--c);
---
+++ exercício3/Program.cs
@@ -0,0 +1,104 @@
+﻿
+namespace exercicio3
+{
+    class Program
+    {
+        static void Main(String[] args)
+
+        /*Fazer um programa onde o usuário irá digitar um valor até o número vinte. 
+         Ou seja, a estrutura de repetição só poderá executar até 20 vezes. A estrutura 
+         de repetição deve verificar os números que são ímpares, pares e múltiplos de 4. 
+         Após o término da estrutura de repetição, o programa deverá exibir qual é o maior 
+         valor computado entre as três questões a serem verificadas. Para isso é importante 
+         que os valores ao serem identificados na estrutura de repetição, haja um somatório dos mesmos.
+        */
+
+
+        {
+            int num, somaIm = 0, somaPar = 0, multQuat = 0;
+
+            Console.WriteLine("Informe um número de 1 a 20");
+
+
+            num = Int16.Parse(Console.ReadLine());
+
+            while (num <= 0 || num >20 )
+            {
+                Console.WriteLine("Informe um numero de 1 até 20");
+                num = Int16.Parse(Console.ReadLine());
+            }
+
+
+            for (int i = num; i < 21; i++)
+            {
+                if (i % 4 == 0)
+                {
+                    multQuat += i;
+                }
+
+                else
+                {
+                    if (i % 2 == 0)
+                    {
+                        somaPar += i;
+                    }
+
+                    else
+                    {
+                        somaIm += i;
+                    }
+                }
+            }
+
+
+            int totalSomaPar = somaPar + multQuat;
+
+            Console.WriteLine();
+
+            Console.WriteLine("Somatório dos números pares a partir do numero " + num + " é: " + totalSomaPar);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Somatório dos números impares a partir do numero " + num + " é: " + somaIm);
+
+            Console.WriteLine();
+
+            Console.WriteLine("Somatório de números múltiplos de 4  a partir do numero " + num + " é: " + multQuat);
+
+            Console.WriteLine();
+
+
+
+            if (somaIm > totalSomaPar && somaIm > multQuat)
+
+            {
+
+                Console.WriteLine("O maior Somatório é dos números ímpares:  " + somaIm);
+
+            }
+
+            else if (totalSomaPar > somaIm && totalSomaPar > multQuat)
+
+            {
+
+                Console.WriteLine("O maior Somatório é dos números pares:  " + totalSomaPar);
+
+            }
+
+            else
+
+
+            {
+
+                Console.WriteLine("O maior Somatório é dos números múltiplos de 4:  " + multQuat);
+
+            }
+
+        }
+
+    }
+}
+
+
+
+
